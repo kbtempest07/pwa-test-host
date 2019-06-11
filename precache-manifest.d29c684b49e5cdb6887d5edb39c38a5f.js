94123bf3cc666e86d493f34b22c711ceecc48592
Karthiklal B <bkarthiklal@techgentsia>
2019-06-11 15:52:20 +0530

    moving build contents to outer folder

---
+++ precache-manifest.d29c684b49e5cdb6887d5edb39c38a5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "32f9611376cc78d70988",
+    "url": "/js/chunk-vendors.d7be0fda.js"
+  },
+  {
+    "revision": "4336a9ff26b08ce17e49",
+    "url": "/js/app.7b4fa9b1.js"
+  },
+  {
+    "revision": "851bafff87e138bcfe30",
+    "url": "/js/about.2a2caafa.js"
+  },
+  {
+    "revision": "96e0d7854214d72b1e4e396731fe88c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4336a9ff26b08ce17e49",
+    "url": "/css/app.6e28d29e.css"
+  }
+];
